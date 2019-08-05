3786bfc688ad0a3a267161dbdf869846f4135b08
choi <kazaana2009@naver.com>
2019-08-05 22:31:37 +0900

    기초2 & 람다 추가

---
+++ Lambda_Interface/src/Ex.java
@@ -0,0 +1,22 @@
+public class Ex {
+    public static void main(String[] args) {
+//        //runnable
+//        Runnable runnable=() -> {
+//          for(int i=0;i<10;i++){
+//              System.out.println("i: "+i);
+//          }
+//        };
+//
+//        Thread thread = new Thread(runnable);
+//        thread.run();
+ 
+        Thread thread = new Thread(()->{
+            for(int i=0;i<10;i++){
+            System.out.println("i: "+i);
+           }
+        });
+
+        thread.start();
+
+    }
+}
